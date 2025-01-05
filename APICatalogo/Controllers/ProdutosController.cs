d75c7921f3649e8f5a76ab9f9c162a76dd7bfa35
henrickmelovital <henrick.melo@yahoo.com.br>
2025-01-05 16:06:33 -0300

    Tipo: ProdutosController - POST, PUT e DELETE Modulo: Criando Web API. Descrição: Criação das rotas para inserir, atualizar e deletar os produtos.

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -15,6 +15,10 @@ namespace APICatalogo.Controllers
             _context = contexto;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public ActionResult<IEnumerable<Produto>> GetProdutos()
         {
@@ -26,7 +30,12 @@ namespace APICatalogo.Controllers
             return produtos;
         }
 
-        [HttpGet("{id:int}")]
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id:int}", Name="ObterProduto")]
         public ActionResult<Produto> Get(int id)
         {
             var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
@@ -35,6 +44,91 @@ namespace APICatalogo.Controllers
                 return NotFound("Produto não encontrado... ");
             }
             return produto;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="produto"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+            {
+                return BadRequest();
+            }
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("obterProduto", new { id = produto.ProdutoId }, produto);
+        }
+
+        /// <summary>
+        ///  Um serviço com o verbo Put atualiza todas as informações.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="produto"></param>
+        /// <returns></returns>
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produto).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+
+        /// <summary>
+        ///  Um serviço com o verbo Patch permite que atualize apenas uma informação de cada vez
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="produto"></param>
+        /// <returns></returns>
+        [HttpPatch("{id:int}")]
+        public ActionResult Patch(int id, Produto produto)
+        {
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+
+            var existingProduto = _context.Produtos.Find(id);
+            if (existingProduto == null)
+            {
+                return NotFound();
+            }
+
+            _context.Entry(existingProduto).CurrentValues.SetValues(produto);
+            _context.SaveChanges();
+
+            return Ok(existingProduto);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if (produto is null)
+            {
+                return NotFound("Produto não localizado...");
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
         }
     }
 }
