cb467817e935fde69ece86ad087e686eee0741a7
henrickmelovital <henrick.melo@yahoo.com.br>
2025-01-05 13:36:32 -0300

    Tipo: Criando o controlador ProdutosController Modulo: Criando Web API. Descrição: Criação da primeira controller para Produtos.

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,19 @@
+﻿using APICatalogo.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : Controller
+    {
+        private readonly APICatalogoContext _context;
+
+        public ProdutosController(APICatalogoContext contexto)
+        {
+            _context = contexto;
+        }
+
+
+    }
+}
