f05ecc3554c0d0077168e1f991fc4a38fb595ce4
henri <henri@HENRICK>
2025-01-05 02:45:11 -0300

    Add project files.

---
+++ APICatalogo/Context/APICatalogoContext.cs
@@ -0,0 +1,16 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context
+{
+    public class APICatalogoContext : DbContext
+    {
+        public APICatalogoContext(DbContextOptions<APICatalogoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> categorias { get; set; }
+    }
+}
---
+++ APICatalogo/Migrations/20250104182402_AjusteDeTabelas.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjusteDeTabelas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Produtos_categorias_CategoriaId",
+                table: "Produtos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_categorias",
+                table: "categorias");
+
+            migrationBuilder.RenameTable(
+                name: "categorias",
+                newName: "Categorias");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Preco",
+                table: "Produtos",
+                type: "decimal(10,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Produtos",
+                type: "nvarchar(80)",
+                maxLength: 80,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagemUrl",
+                table: "Produtos",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Produtos",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Categorias",
+                type: "nvarchar(80)",
+                maxLength: 80,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagemUrl",
+                table: "Categorias",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categorias",
+                table: "Categorias",
+                column: "CategoriaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Produtos_Categorias_CategoriaId",
+                table: "Produtos",
+                column: "CategoriaId",
+                principalTable: "Categorias",
+                principalColumn: "CategoriaId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Produtos_Categorias_CategoriaId",
+                table: "Produtos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categorias",
+                table: "Categorias");
+
+            migrationBuilder.RenameTable(
+                name: "Categorias",
+                newName: "categorias");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Preco",
+                table: "Produtos",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Produtos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(80)",
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagemUrl",
+                table: "Produtos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Produtos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "categorias",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(80)",
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagemUrl",
+                table: "categorias",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_categorias",
+                table: "categorias",
+                column: "CategoriaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Produtos_categorias_CategoriaId",
+                table: "Produtos",
+                column: "CategoriaId",
+                principalTable: "categorias",
+                principalColumn: "CategoriaId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
